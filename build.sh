e0a95c613e93f874bd18c58fd614de344f9a89a0
Starlight Studios <89017509+ThyDarkSorcerer@users.noreply.github.com>
2026-08-25 14:41:30 +0530

    fix(ci): fix meson apt package name and brand lineage version tag as -aperture

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt update
 sudo apt install -y sudo git android-sdk-platform-tools python-is-python3 python3-yaml qemu-utils # libncurses5
 sudo apt install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick protobuf-compiler python3-protobuf lib32readline-dev lib32z1-dev libdw-dev libelf-dev lz4 libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
-sudo apt install -y meson-1.5 glslang-tools python3-mako
+sudo apt install -y meson glslang-tools python3-mako
 git config --global user.name "github-actions[bot]"
 git config --global user.email "github-actions[bot]@users.noreply.github.com"
 git config --global trailer.changeid.key "Change-Id"
@@ -20,7 +20,7 @@ cd android/lineage
 export PATH="$(realpath .)/prebuilts/sdk/tools/linux/bin/:$PATH"
 repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs --no-clone-bundle --depth 1
 repo sync -c --prune --force-sync --retry-fetches=8 -j $(nproc)
-sed -i 's/-$(LINEAGE_BUILDTYPE)/-jqssun/g' vendor/lineage/config/version.mk
+sed -i 's/-$(LINEAGE_BUILDTYPE)/-aperture/g' vendor/lineage/config/version.mk
 
 source build/envsetup.sh
 export AB_OTA_UPDATER=false ROOMSERVICE_BRANCHES="lineage-23.1 lineage-23.0"
